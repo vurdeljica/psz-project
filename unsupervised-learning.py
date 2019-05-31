7a49aee3fd13a6156e8431c8632e8f4069a42d12
Igor Vurdelja <igor.vurdelja@zuehlke.com>
2019-05-31 11:01:04 +0200

    k means ui

--- unsupervised-learning.py
+++ unsupervised-learning.py
@@ -4,10 +4,17 @@ import pandas as pd
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+import seaborn as sns
+from sklearn.decomposition import PCA
+from bokeh.models import HoverTool, BoxZoomTool, PanTool, WheelZoomTool
+from bokeh.plotting import *
+from bokeh.models import ColumnDataSource
+import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-checkbox_columns = ['pera', 'mika', 'zika']
+checkbox_columns = []
 checkbox_values = []
+prepared_data_frame = []#pd.read_csv('C:/Users/igvu/Desktop/Fakultet/Drugi semestar/PSZ/psz-project/albums_df_prepared.csv')
 
 class CheckBoxColumns(tk.Frame):
     def __init__(self, root, *args, **kwargs):
@@ -17,11 +24,12 @@ class CheckBoxColumns(tk.Frame):
         self.root = root
 
         self.vsb = tk.Scrollbar(self, orient="vertical")
-        self.text = tk.Text(self, width=20, height=10,
+        self.text = tk.Text(self, width=20, height=15,
                             yscrollcommand=self.vsb.set)
         self.vsb.config(command=self.text.yview)
         self.vsb.pack(side="right", fill="y")
         self.text.pack(side="left", fill="both", expand=True)
+        checkbox_values = []
 
         for index, column in enumerate(checkbox_columns):
             var = tk.BooleanVar()
@@ -34,7 +42,7 @@ class CheckBoxColumns(tk.Frame):
 
 root = tk.Tk()
 
-canvas1 = tk.Canvas(root, width=400, height=400, relief='raised')
+canvas1 = tk.Canvas(root, width=400, height=500, relief='raised')
 canvas1.pack()
 
 label1 = tk.Label(root, text='k-Means Clustering')
@@ -52,11 +60,11 @@ canvas1.create_window(200, 140, window=entry1)
 def getExcel():
     global df
     global checkbox_columns
+    global prepared_data_frame
     import_file_path = filedialog.askopenfilename()
-    #read_file = pd.read_excel(import_file_path)
-    #df = DataFrame(read_file, columns=['x', 'y'])
-    df = pd.read_csv(import_file_path)
-    checkbox_columns = list(df.columns.values)
+    prepared_data_frame = pd.read_csv(import_file_path)
+    checkbox_columns = list(prepared_data_frame.columns.values)
+    checkbox_columns = list(filter(lambda column: "generated" not in column, checkbox_columns))
     canvas1.create_window(117, 200, window=CheckBoxColumns(root), anchor='nw')
 
 
@@ -67,9 +75,77 @@ canvas1.create_window(200, 70, window=browseButtonExcel)
 def getKMeans():
     global df
     global numberOfClusters
-    #numberOfClusters = int(entry1.get())
+    global checkbox_columns
+    global checkbox_values
+    numberOfClusters = int(entry1.get())
+    #numberOfClusters = 10
 
-    #kmeans = KMeans(n_clusters=numberOfClusters).fit(df)
+    selected_columns = []
+    for index, checkbox_value in enumerate(checkbox_values):
+        if checkbox_value.get() == True:
+            selected_columns.append(checkbox_columns[index])
+
+    print(selected_columns)
+
+    info_data_frame = pd.DataFrame()
+
+    prepared_columns = list(prepared_data_frame.columns.values)
+    columns_for_model = []
+    for column in selected_columns:
+        for prepared_column in prepared_columns:
+            if (column in prepared_column) and (column != prepared_column):
+                columns_for_model.append(prepared_column)
+            elif column == prepared_column:
+                info_data_frame[column] = prepared_data_frame[prepared_column]
+
+    #columns_for_model = [column for column in columns if column in selected_columns]
+    print (columns_for_model)
+
+    features_data_frame = pd.DataFrame()
+    for column in columns_for_model:
+        features_data_frame[column] = prepared_data_frame[column]
+
+    clustering_kmeans = KMeans(n_clusters=numberOfClusters, precompute_distances='auto')
+    features_data_frame['clusters'] = clustering_kmeans.fit_predict(features_data_frame)
+    features_data_frame.to_csv('kmeans.csv')
+
+    reduced_data = PCA(n_components=2).fit_transform(features_data_frame)
+    results = pd.DataFrame(reduced_data, columns=['pca1', 'pca2'])
+    info_data_frame['pca1'] = results['pca1']
+    info_data_frame['pca2'] = results['pca2']
+    info_data_frame['clusters'] = features_data_frame['clusters']
+    results.to_csv('pca.csv')
+
+
+    cluster_colors = [list(np.random.choice(range(256), size=3)) for _ in range(numberOfClusters)]
+    cluster_colors = ["#{0:02x}{1:02x}{2:02x}".format(cluster_color[0], cluster_color[1], cluster_color[2]) for cluster_color in cluster_colors]
+    colors = []
+    for row in info_data_frame['clusters']:
+        colors.append(cluster_colors[row])
+        #colors.append('red')
+    info_data_frame['color']=colors
+    print(colors)
+
+    #numinfo_data_frame[['col1', 'col2', 'col3', 'col4']].groupby(['col1', 'col2']).agg(['mean', 'count'])
+
+    output_file("kmeans.html")
+    source = ColumnDataSource(info_data_frame)
+    tooltips_tuples = []
+    for column in list(info_data_frame.columns.values):
+        tuple = (column, '@' + column)
+        tooltips_tuples.append(tuple)
+
+    hover = HoverTool(tooltips=tooltips_tuples)
+    p = figure(plot_width=800, plot_height=800, tools=[hover, BoxZoomTool(), PanTool(), WheelZoomTool()])
+    p.circle('pca1', 'pca2', size=7, source=source, color='color', fill_alpha=0.2)
+
+    show(p)
+
+
+    #sns.scatterplot(x="pca1", y="pca2", hue=features_data_frame['clusters'], data=results)
+    #plt.title('K-means Clustering with 2 dimensions')
+    #plt.show()
+
     #centroids = kmeans.cluster_centers_
 
     #label3 = tk.Label(root, text=centroids)
