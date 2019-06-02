d25b8d80e18b3b5ed25ba3e1be59f970c6fd43af
Igor Vurdelja <igor.vurdelja@zuehlke.com>
2019-06-02 17:36:10 +0200

    fixed small issue in resolving songs ids

--- statistics/resolve_addresses.py
+++ statistics/resolve_addresses.py
@@ -21,11 +21,6 @@ cnx = sqlite3.connect(absolute_path_to_database)
 
 song_df = pd.read_sql_query("SELECT * FROM song", cnx)
 
-
-cnx2 = sqlite3.connect('C:/Users/igvu/Desktop/discogs_baze/bazeTestiranje/discogs_new_all.db')
-song_df2 = pd.read_sql_query("SELECT * FROM song", cnx2)
-song_df['song_id_2'] = song_df2['song_id']
-
 list_of_translated_addresses = []
 list_of_jobs = []
 
@@ -66,7 +61,7 @@ def do_work():
                     'https': random.choice(proxies_list)
                 }
 
-                url_full = requests.get('https://www.discogs.com/track/' + url_base, proxies=proxies, headers=headers, timeout=20)
+                url_full = requests.get('https://www.discogs.com/track/' + url_base, proxies=proxies, headers=headers, timeout=50)
                 status_code = url_full.status_code
 
                 if status_code == 429 or "Response" in url_full:
@@ -93,13 +88,13 @@ def do_work():
 
         LOCK.release()
 
-        if (url_full != '--')  and ("Response" not in url_full):
+        if (url_full != '--') and ("/track" not in url_full) and ("Response" not in url_full):
             list_of_translated_addresses_per_thread.append((url_base, url_full))
         else:
             list_of_jobs_per_thread.append(url_base)
 
         time_now = time.time()
-        if (time_now - start > 300):
+        if (time_now - start > 100):
             LOCK.acquire()
             list_of_translated_addresses += list_of_translated_addresses_per_thread
             LOCK.release()
@@ -113,13 +108,10 @@ def do_work():
     LOCK.release()
 
 print(len(song_df[song_df.song_id.str.contains("/composition")]))
-for index, row in song_df.iterrows():
-    if '/composition/' not in row['song_id']:
-        list_of_song_id.append(row['song_id_2'])
 
-#for song_id in song_df['song_id']:
-#    if '/composition/' not in song_id:
-#        list_of_song_id.append(song_id)
+for song_id in song_df['song_id']:
+    if '/composition/' not in song_id:
+        list_of_song_id.append(song_id)
 
 list_of_jobs = list(numpy.array_split(numpy.array(list_of_song_id), concurrent * 2))
 list_of_jobs = [list(x) for x in list_of_jobs]
@@ -142,67 +134,11 @@ try:
         for translated_address in list_of_translated_addresses_local:
             url_base = translated_address[0]
             url = translated_address[1]
-            #song_df.loc[song_df['song_id'] == url_base,'song_id'] = url
-            song_df.loc[song_df['song_id_2'] == url_base, 'song_id'] = url
+            song_df.loc[song_df['song_id'] == url_base,'song_id'] = url
 
         song_df = song_df.astype(str)
         song_df.to_sql('song', cnx, if_exists='replace', index=False)
 
 except KeyboardInterrupt:
-    del song_df['song_id_2']
-    song_df = song_df.astype(str)
-    song_df.to_sql('song', cnx, if_exists='replace', index=False)
     pass
 
-del song_df['song_id_2']
-song_df = song_df.astype(str)
-song_df.to_sql('song', cnx, if_exists='replace', index=False)
-
-
-#print(len(song_df[song_df.song_id.str.contains("/composition")]))
-
-#done = 0
-#total = 0
-#for index, row in song_df.iterrows():
-#    num_of_exceptions = 0
-#    if "/composition" not in row['song_id']:
-#        while num_of_exceptions < 100:
-#            status_code = 0
-#            try:
-#                headers = {
-#                    'User-Agent': "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"}#
-#
-#                proxies = {
-#                    'https': random.choice(proxies_list)
-#                }
-#
-#                url_full = requests.get('https://www.discogs.com/track/' + row['song_id_2'], proxies=proxies, headers=headers, timeout=3)
-#                status_code = url_full.status_code
-#
-#               if status_code == 429 or "Response" in url_full:
-#                    raise ValueError('429 Error')
-#
-#                conn = url_full
-#                url_full = url_full.url
-#                print(url_full)
-#                done += 1
-#                total += 1
-#                song_df.loc[song_df['song_id_2'] == row['song_id_2'], 'song_id'] = url_full
-#                conn.close()
-#
-#                break
-#            except Exception as e:
-#                if num_of_exceptions == 100 and status_code == 429:
-#                    print("going to sleep...")
-#                    sleep(11)
-#                    num_of_exceptions = 0
-#
-#                num_of_exceptions = num_of_exceptions + 1
-#
-#           total += 1
-#            print(done, "/", total)
-#           song_df.loc[song_df['song_id_2'] == row['song_id_2'], 'song_id'] = row['song_id_2']
-#
-#del song_df['song_id_2']
-#song_df = song_df.astype(str)
-#song_df.to_sql('song', cnx, if_exists='replace', index=False)
